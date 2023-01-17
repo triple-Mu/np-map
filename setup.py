45f08cb97376819b362254cf38049953febf670f
triple-Mu <gpu@163.com>
2023-01-17 16:12:52 +0800

    Format

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 import setuptools
+
 from npmap import __version__
 
 with open('README.md') as f:
     long_description = f.read()
 
-setuptools.setup(name='pymap',
-                 version=__version__,
-                 author='triplemu',
-                 author_email='gpu@163.com',
-                 description='A simple tool for calculating object detection map',
-                 long_description=long_description,
-                 long_description_content_type='text/markdown',
-                 packages=setuptools.find_packages(),
-                 classifiers=[
-                     'Programming Language :: Python :: 3',
-                     'License :: OSI Approved :: MIT License',
-                     'Operating System :: OS Independent',
-                 ],
-                 install_requires=[
-                     'numpy',
-                 ],
-                 python_requires='>=3')
+setuptools.setup(
+    name='npmap',
+    version=__version__,
+    author='triplemu',
+    author_email='gpu@163.com',
+    description='A simple tool for calculating object detection map',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'numpy',
+    ],
+    python_requires='>=3')
